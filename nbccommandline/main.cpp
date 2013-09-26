1728fcfa6b668cf775f4b62a06fb697d28aa9867
Tugrul Savran <tugrul@Tugruls-MacBook-Pro.local>
2013-09-26 18:50:25 +0300

    Initial Commit

---
+++ nbccommandline/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  nbccommandline
+//
+//  Created by Tugrul Savran on 9/26/13.
+//  Copyright (c) 2013 CuriouScientists. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
